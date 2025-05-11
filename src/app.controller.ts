dca9eb36fb3903cef9406ae227c6ef106bc2d80b
Renato Machado <renatomachado176@gmail.com>
2025-05-11 18:41:10 +0100

    [chore]: Prisma Service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common'
-import type { AppService } from './app.service'
+import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService
+  ) {}
 
   @Get('/hello')
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Get('/users')
+  async getUsers() {
+    return await this.prisma.user.findMany()
   }
 }
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from 'generated/prisma'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ['warn', 'error'],
+    })
+  }
+  onModuleInit() {
+    return this.$connect()
+  }
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
