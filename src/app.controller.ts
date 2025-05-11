65b25f82091d590202e4d8f57487b5e06368b148
Renato Machado <renatomachado176@gmail.com>
2025-05-11 17:47:37 +0100

    [chore]: Biome added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import type { AppService } from './app.service'
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
