3da10c146b374c3a06fd966b09cc5f0132538b1d
Renato Machado <renatomachado176@gmail.com>
2025-05-12 13:48:46 +0100

    [feat]: Authenticate Controller

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,53 @@
+import {
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { compare, hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { JwtService } from '@nestjs/jwt'
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService
+  ) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body
+
+    const userWithEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!userWithEmail) {
+      throw new UnauthorizedException('User credentials do not match.')
+    }
+
+    const isPasswordValid = await compare(password, userWithEmail.password)
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('User credentials do not match.')
+    }
+
+    const accessToken = this.jwt.sign({ sub: userWithEmail.id })
+
+    return { access_token: accessToken }
+  }
+}
