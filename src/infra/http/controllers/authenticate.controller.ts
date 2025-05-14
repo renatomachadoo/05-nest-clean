313f6f2cf028e713748411376bb5530f90fc0475
Renato Machado <renatomachado176@gmail.com>
2025-05-14 21:21:15 +0100

    [feat]: Controllers error handling

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -1,8 +1,13 @@
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { UsePipes } from '@nestjs/common'
+import {
+  BadRequestException,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
 import { Body, Controller, Post } from '@nestjs/common'
 import { z } from 'zod'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { WrongCredentialsError } from '@/domain/forum/application/use-cases/errors/wrong-credentials-error'
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
@@ -23,7 +28,14 @@ export class AuthenticateController {
     const result = await this.authenticateStudent.execute({ email, password })
 
     if (result.isLeft()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case WrongCredentialsError:
+          throw new UnauthorizedException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
 
     const { accessToken } = result.value
