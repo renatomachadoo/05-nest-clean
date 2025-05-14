8d57b1b3fe46e8155b86141b1f0cce9ebc019354
Renato Machado <renatomachado176@gmail.com>
2025-05-14 16:58:35 +0100

    [chore]: Added email and password to student entity

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,11 +3,21 @@ import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
   get name() {
     return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
   }
 
   static create(props: StudentProps, id?: UniqueEntityId) {
