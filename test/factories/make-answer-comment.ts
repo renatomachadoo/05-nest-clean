0c0d5770acc1f66488565be8dd582ef0b3671612
Renato Machado <renatomachado176@gmail.com>
2025-05-15 21:14:32 +0100

    [test]: Factories E2E

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -3,7 +3,10 @@ import {
   AnswerComment,
   type AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswerComment(
   override?: Partial<AnswerCommentProps>,
@@ -20,4 +23,21 @@ export function makeAnswerComment(
   )
 
   return answerComment
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(
+    data: Partial<AnswerCommentProps> = {}
+  ): Promise<AnswerComment> {
+    const answerComment = makeAnswerComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    })
+
+    return answerComment
+  }
 }
--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -3,7 +3,10 @@ import {
   Answer,
   type AnswerProps,
 } from '@/domain/forum/enterprise/entities/answer'
+import { PrismaAnswerMapper } from '@/infra/database/prisma/mappers/prisma-answer-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswer(
   override?: Partial<AnswerProps>,
@@ -20,4 +23,19 @@ export function makeAnswer(
   )
 
   return answer
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(data: Partial<AnswerProps> = {}): Promise<Answer> {
+    const answer = makeAnswer(data)
+
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    })
+
+    return answer
+  }
 }
